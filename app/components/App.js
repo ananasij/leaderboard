555e2d1365034549eb569b78f2b453e34870983a
Hanna Senkevich <hanna.senkevich@gmail.com>
2017-03-28 16:30:30 +0200

    Initial commit

---
+++ app/components/App.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var App = React.createClass({
+
+    render: function() {
+        return (
+            <div className='container'>lol</div>
+        );
+    }
+});
+
+module.exports = App;
