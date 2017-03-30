bab824d9843e389ee672404667d40b5170c283a7
Hanna Senkevich <hanna.senkevich@gmail.com>
2017-03-30 19:48:10 +0200

    Displaying 30-day leaderboard with basic styling

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,15 @@
 var React = require('react');
+var LeaderboardGetter = require('./LeaderboardGetter');
 
 var App = React.createClass({
-
     render: function() {
         return (
-            <div className='container'>lol</div>
+            <div className='container'>
+                Sort by:
+                <button value='30d'>Past 30 days points</button>
+                <button value='total'>All time points</button>
+                <LeaderboardGetter />
+            </div>
         );
     }
 });
---
+++ app/components/LeaderboardView.js
@@ -0,0 +1,41 @@
+var React = require('react');
+
+var LeaderboardView = React.createClass({
+    buildLeaderboard: function(rawLeaderboard) {
+        var processedLeaderboard = [];
+        var rank = 0;
+        rawLeaderboard.map(function(line) {
+            rank += 1;
+            processedLeaderboard.push(
+                <tr>
+                    <td>{rank}</td>
+                    <td><img src={line.img} alt={line.username + "'s userpicture"} className="userimage"/>
+                        <a href={'https://www.freecodecamp.com/' + line.username}>{line.username}</a></td>
+                    <td>{line.recent}</td>
+                    <td>{line.alltime}</td>
+                </tr>
+            );
+            return true;
+        });
+        return processedLeaderboard;
+    },
+    render: function() {
+        return <div>
+            <table className="table table-striped">
+                <thead>
+                <tr>
+                    <th>Rank</th>
+                    <th>Camper</th>
+                    <th>Points for past 30 days</th>
+                    <th>Points for all days</th>
+                </tr>
+                </thead>
+                <tbody>
+                {this.buildLeaderboard(this.props.currentLeaderboard)}
+                </tbody>
+            </table>
+        </div>;
+    }
+});
+
+module.exports = LeaderboardView;
