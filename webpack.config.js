0f1d5fc3cb3faec09a5bfcb2615ae4b3a97927fc
Hanna Senkevich <hanna.senkevich@gmail.com>
2017-05-17 18:26:15 +0200

    Prepared app for publishing to gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,50 @@ var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
     filename: 'index.html',
     inject: 'body'
 });
+
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var ExtractTextPluginConfig = new ExtractTextPlugin({ // define where to save the file
+    filename: 'dist/[name].bundle.css',
+    allChunks: true
+});
+
 module.exports = {
-    entry: __dirname + '/app/index.js',
+    entry: [__dirname + '/app/index.js', __dirname + '/app/styles/main.scss'],
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader'
-            }
+                use: 'babel-loader'
+            },
+            { // regular css files
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    use: 'css-loader?importLoaders=1'
+                })
+            },
+            { // sass / scss loader for webpack
+                test: /\.(sass|scss)$/,
+                use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+            },
+            {
+                test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[path][name].[ext]'
+                    }
+                }]
+            },
         ]
     },
     output: {
         filename: 'transformed.js',
         path: __dirname + '/build'
     },
-    plugins: [HTMLWebpackPluginConfig],
+    plugins: [
+        HTMLWebpackPluginConfig,
+        ExtractTextPluginConfig
+    ],
     devtool: 'source-map'
 };
